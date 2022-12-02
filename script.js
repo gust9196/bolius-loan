b1ad3406b259c2492c9ad9920e00290fc40ccb29
gust9196 <gust9196@stud.kea.dk>
2022-12-02 11:26:22 +0100

    freddy for pokker

---
+++ script.js
@@ -0,0 +1,6 @@
+function multiplyBy()
+{
+    num1 = parseInt(document.getElementById("firstNumber").value);
+    num2 = parseInt(document.getElementById("secondNumber").value);
+    document.getElementById("result").innerHTML =  num1 * 4 + num2;
+}
